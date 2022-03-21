92cc263b4d584c4bfdc8f3cbe2d377af0c5edc6f
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-21 18:30:34 +0530

    Finalized the app

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import classes from './Input.module.css';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useGlobalContext } from '../context.js';
 
 const Input = () => {
+  const { name, setName, list, setList, completed } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    else {
+      const newItem = { id: new Date().getTime().toString(), content: name, completed };
+      setList([...list, newItem]);
+    }
+    setName('');
+  };
+
   return (
     <>
-      <form className={classes.form}>
-        <input className={classes.input} type="text" placeholder="Add items ...." />
-        <button className={classes.btn}>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Add items ...."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className={classes.btn}>
           <AiOutlinePlusCircle className={classes['add-btn']} />
         </button>
       </form>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ListItem from './ListItem';
 import classes from './List.module.css';
+import { useGlobalContext } from '../context';
 
 const List = () => {
-  return (
-    <ul>
-      <ListItem />
-    </ul>
-  );
+  const { list } = useGlobalContext();
+  {
+    if (list.length === 0)
+      return (
+        <div>
+          <h3>Empty List</h3>
+        </div>
+      );
+    return (
+      <ul className={classes.list}>
+        {list.map((el) => {
+          return <ListItem item={el} key={el.id} />;
+        })}
+      </ul>
+    );
+  }
 };
 
 export default List;
