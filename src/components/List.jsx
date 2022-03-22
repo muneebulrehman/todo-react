fd04f41d6cb10c3cb1761c9d5c86d8a02805a105
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-22 15:18:53 +0530

    implemented changes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,21 +5,19 @@ import { useGlobalContext } from '../context';
 
 const List = () => {
   const { list } = useGlobalContext();
-  {
-    if (list.length === 0)
-      return (
-        <div>
-          <h3>Empty List</h3>
-        </div>
-      );
+  if (list.length === 0)
     return (
-      <ul className={classes.list}>
-        {list.map((el) => {
-          return <ListItem item={el} key={el.id} />;
-        })}
-      </ul>
+      <div>
+        <h3>Empty List</h3>
+      </div>
     );
-  }
+  return (
+    <ul className={classes.list}>
+      {list.map((el) => {
+        return <ListItem item={el} key={el.id} />;
+      })}
+    </ul>
+  );
 };
 
 export default List;
